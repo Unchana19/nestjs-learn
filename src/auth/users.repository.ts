e646294ba774246ce652f1fb84495505efe9b667
annchana <annchanaoat@gmail.com>
2024-08-24 22:30:57 +0700

    auth

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,37 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+export class UsersRepository {
+  constructor(
+    @InjectRepository(User)
+    public readonly repository: Repository<User>,
+  ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    try {
+      const user = this.repository.create({
+        username,
+        password: hashedPassword,
+      });
+      await this.repository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
